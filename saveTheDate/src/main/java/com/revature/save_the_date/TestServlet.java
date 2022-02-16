a79058737423ad9c2d4142264f35e5629c1e0d94
Scott Damon <scott.m.damon1@gmail.com>
2022-02-15 20:56:13 -0500

    Created a functioning test servlet. Also edited .gitignore to include bin and resources folders, classes, and properties

---
+++ saveTheDate/src/main/java/com/revature/save_the_date/TestServlet.java
@@ -0,0 +1,15 @@
+package com.revature.save_the_date;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("<h1>Oh it works. weeeeeee</h1>");
+	}
+}
