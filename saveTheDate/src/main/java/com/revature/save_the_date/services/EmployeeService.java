864462c3c2f038510665846c53804473ccb10189
Scott Damon <scott.m.damon1@gmail.com>
2022-02-17 11:37:07 -0500

    edited CLL and created WeddingServlet and Service DAO

--- saveTheDate/src/main/java/com/revature/save_the_date/services/EmployeeService.java
+++ saveTheDate/src/main/java/com/revature/save_the_date/services/EmployeeService.java
@@ -1,5 +1,12 @@
 package com.revature.save_the_date.services;
 
+import com.revature.save_the_date.dao.EmployeeDAO;
+
 public class EmployeeService {
+
+	public EmployeeService(EmployeeDAO employeeDAO, WeddingService weddingService) {
+		// TODO Auto-generated constructor stub
+	}
+
 
 }
