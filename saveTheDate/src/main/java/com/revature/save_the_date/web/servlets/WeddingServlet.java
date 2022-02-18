58031994e1068b167111a8342fdab25057b04628
ravaney <lewislamar20@outlook.com>
2022-02-18 10:40:48 -0500

    added employee page to add venu, florist etc

--- saveTheDate/src/main/java/com/revature/save_the_date/web/servlets/WeddingServlet.java
+++ saveTheDate/src/main/java/com/revature/save_the_date/web/servlets/WeddingServlet.java
@@ -1,5 +1,42 @@
 package com.revature.save_the_date.web.servlets;
 
-public class WeddingServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class WeddingServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.getWriter().write("<head><title>Save The Date !</title></head>"
+				+ "<img src='./images/savethedate.png' alt='logos'/>"
+				+ "<body>"
+				+ "<h1> Home </h1>"
+				+ "<div id='intro'> "
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				+ ""
+				
+				// CSS
+				
+				+ "<style type=text/css>"
+				+ "body {background-color: #79525C;}"
+				+ ""
+				+ "</body>"
+				+ "</style>");
+	}
 
 }
