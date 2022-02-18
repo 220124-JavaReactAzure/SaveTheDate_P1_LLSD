073ed76ed89001113b0c8ada7f9a1cd6c4ad7888
Scott Damon <scott.m.damon1@gmail.com>
2022-02-18 10:50:15 -0500

    Fixed typo in RegistryServlet

--- saveTheDate/src/main/java/com/revature/save_the_date/web/servlets/RegistryServlet.java
+++ saveTheDate/src/main/java/com/revature/save_the_date/web/servlets/RegistryServlet.java
@@ -1,18 +1,5 @@
 package com.revature.save_the_date.web.servlets;
 
-<<<<<<< HEAD
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-import com.revature.save_the_date.services.RegistryService;
-import com.revature.save_the_date.services.WeddingService;
-
-@WebServlet(value="/registry")
-public class RegistryServlet extends HttpServlet{
-
-	public RegistryServlet(RegistryService registryService) {
-		// TODO Auto-generated constructor stub
-=======
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -21,13 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.revature.save_the_date.services.RegistryService;
+import com.revature.save_the_date.services.WeddingService;
+
 @WebServlet(value="/registry")
 public class RegistryServlet extends HttpServlet{
 	
+	public RegistryServlet(RegistryService registryService) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.getWriter().write("helooooooooooooooooooooooooooo");
->>>>>>> fa9125de520be592552159cca7d4ca0910b3dee8
 	}
 
 }
